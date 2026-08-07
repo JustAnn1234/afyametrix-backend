4bb45f1c8cc1ccc8f2c847c95fc901e55f892f71
Saronzeleke <Cassiopiasaron@gmail.com>
2026-08-07 18:21:28 +0300

    main_production

--- api/main_production.py
+++ api/main_production.py
@@ -180,7 +180,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://your-production-domain.com"  # Add your production domain
+        "https://afyametrix.netlify.app"
+  # Add your production domain
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
